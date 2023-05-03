cf9259b3adbcc6473ecd9f21fcbd6ad8d4c5db0e
Dan Santos <dan.santos.hd@outlook.com>
2023-05-02 23:01:47 -0400

    Se modifica Main para crear las polilineas y los conjunto de segmentos

--- bentley-ottmann/src/bentley_ottmann/Main.java
+++ bentley-ottmann/src/bentley_ottmann/Main.java
@@ -13,11 +13,48 @@ public class Main {
         double range_min = 100;
         double range_max = 900;
 
+        Point p1 = new Point(550, 200);
+        Point p2 = new Point(650, 450);
+        Point p3 = new Point(450, 700);
+        Point p4 = new Point(750, 850);
+        Polyline polyline = new Polyline(Arrays.asList(p1, p2, p3, p4));
+        ArrayList<Polyline> polylines = new ArrayList<>();
+        polylines.add(polyline);
+
         ArrayList<Segment> data = new ArrayList<>();
-        for(int i = 0; i < 100; i++) {
+        /*
+        for(int i = 0; i < 4; i++) {
             Point p_1 = new Point(rand(range_min, range_max), rand(range_min, range_max));
             Point p_2 = new Point(rand(range_min, range_max), rand(range_min, range_max));
             data.add(new Segment(p_1, p_2));
+        }
+         */
+
+        /*
+        int numberOfPointsPerSide = 4;
+        double intervalRange = (range_max - range_min) / (numberOfPointsPerSide + 1);
+        for(int i = 0; i < numberOfPointsPerSide; i++) {
+            Point p_1 = new Point(200, (i + 1) * intervalRange);
+            Point p_2 = new Point(700, (i + 2) * intervalRange);
+            data.add(new Segment(p_1, p_2));
+        }*/
+
+        int numberOfPointsPerSide = 4;
+        double intervalRange = (range_max - range_min) / (numberOfPointsPerSide + 1);
+        List<Point> points = new ArrayList<>();
+        for(int i = 0; i < numberOfPointsPerSide; i++) {
+            Point p_1 = new Point(200 + (i * 10), (i + 1) * intervalRange);
+            Point p_2 = new Point(700 + (i * 10), (i + 2) * intervalRange);
+            points.add(p_1);
+            points.add(p_2);
+        }
+
+        for (int i = 0; i < points.size(); ++i) {
+            for (int j = i + 1; j < points.size(); ++j) {
+                Point p_1 = points.get(i);
+                Point p_2 = points.get(j);
+                data.add(new Segment(p_1, p_2));
+            }
         }
 
         BentleyOttmann test = new BentleyOttmann(data);
@@ -29,7 +66,7 @@ public class Main {
         //test.print_intersections();
         ArrayList<Point> intersections = test.get_intersections();
 
-        new GUI(data, intersections);
+        new GUI(data, intersections, polylines);
 
         System.out.println("number of intersections: " + intersections.size());
         System.out.println("runtime: " + (t2 - t1) + " ms");
