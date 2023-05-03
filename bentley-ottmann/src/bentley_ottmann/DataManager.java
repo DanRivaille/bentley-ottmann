0dfb426ba7ab4f3d60bb37104aa94d668b6895a2
Dan Santos <dan.santos.hd@outlook.com>
2023-05-03 13:54:22 -0400

    Se agrega una clase DataManager que guarda y carga los datos, servira para ir probando instancias en especifico

---
+++ bentley-ottmann/src/bentley_ottmann/DataManager.java
@@ -0,0 +1,61 @@
+package bentley_ottmann;
+
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DataManager {
+
+    private String filename = "C:\\Users\\Spider.cl\\Repositories\\points.csv";
+
+    public void savePoints(ArrayList<Segment> data) {
+        try {
+            File csvFile = new File(this.filename);
+            FileWriter fileWriter = new FileWriter(csvFile);
+
+            for (Segment segment: data) {
+
+                String line = String.valueOf(segment.first().get_x_coord()) +
+                        ';' +
+                        segment.first().get_y_coord() +
+                        ';' +
+                        segment.second().get_x_coord() +
+                        ';' +
+                        segment.second().get_y_coord() +
+                        '\n';
+                fileWriter.write(line);
+            }
+
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public ArrayList<Segment> loadPoints() {
+        ArrayList<Segment> data = new ArrayList<>();
+
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(this.filename));
+
+            for (String line : allLines) {
+                String[] valuesStringList = line.split(";");
+
+                Point p1 = new Point(Double.parseDouble(valuesStringList[0]), Double.parseDouble(valuesStringList[1]));
+                Point p2 = new Point(Double.parseDouble(valuesStringList[2]), Double.parseDouble(valuesStringList[3]));
+                data.add(new Segment(p1, p2));
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        return data;
+    }
+}
