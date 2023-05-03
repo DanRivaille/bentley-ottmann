75e858afe99110c304fec7e7e8aa7ab1c4a470ee
Dan Santos <dan.santos.hd@outlook.com>
2023-05-02 23:13:08 -0400

    Se refactoriza el codigo para comenzar a editar la logica

--- bentley-ottmann/src/bentley_ottmann/Main.java
+++ bentley-ottmann/src/bentley_ottmann/Main.java
@@ -7,11 +7,12 @@ import java.util.*;
  */
 
 public class Main {
+    static double range_min = 100;
+    static double range_max = 900;
 
     public static void main(String[] args) {
 
-        double range_min = 100;
-        double range_max = 900;
+
 
         Point p1 = new Point(550, 200);
         Point p2 = new Point(650, 450);
@@ -22,38 +23,19 @@ public class Main {
         polylines.add(polyline);
 
         ArrayList<Segment> data = new ArrayList<>();
-        /*
-        for(int i = 0; i < 4; i++) {
-            Point p_1 = new Point(rand(range_min, range_max), rand(range_min, range_max));
-            Point p_2 = new Point(rand(range_min, range_max), rand(range_min, range_max));
-            data.add(new Segment(p_1, p_2));
-        }
-         */
+        int aux = 1;
 
-        /*
-        int numberOfPointsPerSide = 4;
-        double intervalRange = (range_max - range_min) / (numberOfPointsPerSide + 1);
-        for(int i = 0; i < numberOfPointsPerSide; i++) {
-            Point p_1 = new Point(200, (i + 1) * intervalRange);
-            Point p_2 = new Point(700, (i + 2) * intervalRange);
-            data.add(new Segment(p_1, p_2));
-        }*/
-
-        int numberOfPointsPerSide = 4;
-        double intervalRange = (range_max - range_min) / (numberOfPointsPerSide + 1);
-        List<Point> points = new ArrayList<>();
-        for(int i = 0; i < numberOfPointsPerSide; i++) {
-            Point p_1 = new Point(200 + (i * 10), (i + 1) * intervalRange);
-            Point p_2 = new Point(700 + (i * 10), (i + 2) * intervalRange);
-            points.add(p_1);
-            points.add(p_2);
+        switch (aux) {
+            case 0 -> addPoints1(data);
+            case 1 -> addPoints2(data);
+            case 2 -> addPoints3(data);
         }
 
-        for (int i = 0; i < points.size(); ++i) {
-            for (int j = i + 1; j < points.size(); ++j) {
-                Point p_1 = points.get(i);
-                Point p_2 = points.get(j);
-                data.add(new Segment(p_1, p_2));
+        for (Polyline pl: polylines) {
+            for (int i = 1; i < pl.getPointCount(); ++i) {
+                Point point1 = pl.getPoint(i - 1);
+                Point point2 = pl.getPoint(i);
+                data.add(new Segment(point1, point2));
             }
         }
 
@@ -77,4 +59,42 @@ public class Main {
         return range_min + (r.nextDouble() * (range_max - range_min));
     }
 
+
+    private static void addPoints1(ArrayList<Segment> data) {
+        for(int i = 0; i < 4; i++) {
+            Point p_1 = new Point(rand(range_min, range_max), rand(range_min, range_max));
+            Point p_2 = new Point(rand(range_min, range_max), rand(range_min, range_max));
+            data.add(new Segment(p_1, p_2));
+        }
+    }
+
+    private static void addPoints2(ArrayList<Segment> data) {
+        int numberOfPointsPerSide = 4;
+        double intervalRange = (range_max - range_min) / (numberOfPointsPerSide + 1);
+        for(int i = 0; i < numberOfPointsPerSide; i++) {
+            Point p_1 = new Point(200, (i + 1) * intervalRange);
+            Point p_2 = new Point(700, (i + 2) * intervalRange);
+            data.add(new Segment(p_1, p_2));
+        }
+    }
+
+    private static void addPoints3(ArrayList<Segment> data) {
+        int numberOfPointsPerSide = 4;
+        double intervalRange = (range_max - range_min) / (numberOfPointsPerSide + 1);
+        List<Point> points = new ArrayList<>();
+        for(int i = 0; i < numberOfPointsPerSide; i++) {
+            Point p_1 = new Point(200 + (i * 10), (i + 1) * intervalRange);
+            Point p_2 = new Point(700 + (i * 10), (i + 2) * intervalRange);
+            points.add(p_1);
+            points.add(p_2);
+        }
+
+        for (int i = 0; i < points.size(); ++i) {
+            for (int j = i + 1; j < points.size(); ++j) {
+                Point p_1 = points.get(i);
+                Point p_2 = points.get(j);
+                data.add(new Segment(p_1, p_2));
+            }
+        }
+    }
 }
