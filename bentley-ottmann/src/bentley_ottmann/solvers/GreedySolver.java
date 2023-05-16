05392ddb0945213687f09a7b0c6e3a3839b75052
Dan Santos <dan.santos.hd@outlook.com>
2023-05-15 20:50:19 -0400

    Se crea la clase GreedySolver

---
+++ bentley-ottmann/src/bentley_ottmann/solvers/GreedySolver.java
@@ -0,0 +1,20 @@
+package bentley_ottmann.solvers;
+
+import bentley_ottmann.IntersectionsSolver;
+import bentley_ottmann.Point;
+
+import java.util.ArrayList;
+
+public class GreedySolver implements IntersectionsSolver {
+
+
+    @Override
+    public void findIntersections() {
+
+    }
+
+    @Override
+    public ArrayList<Point> getIntersections() {
+        return null;
+    }
+}
